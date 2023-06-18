d722bb0cc37b1709febfa63015f0fa5f1ff5b380
keemsy <phzzz@naver.com>
2023-06-18 20:06:10 +0900

    Add: MemberController 및 에러 핸들러 추가 - REST API 통신 간 에러 핸들링을 위한 GlobalExceptionHandler 및 ErrorDTO 추가 -

---
+++ src/main/java/com/shoes/ordering/system/common/handler/GlobalExceptionHandler.java
@@ -0,0 +1,61 @@
+package com.shoes.ordering.system.common.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.ValidationException;
+import java.util.stream.Collectors;
+
+// REST API 모듈에서 사용 가능하다.
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseBody
+    @ExceptionHandler(value = {Exception.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorDTO handlerException(Exception exception) {
+        log.error(exception.getMessage(), exception);
+        return ErrorDTO.builder()
+                .code(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
+                .message("Unexpected error!")
+                .build();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(value = {ValidationException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDTO handlerException(ValidationException validationException) {
+        ErrorDTO errorDTO;
+        if (validationException instanceof ConstraintViolationException) {
+            String violations = extractViolationsFromException((ConstraintViolationException) validationException);
+            log.error(violations, validationException);
+            errorDTO = ErrorDTO.builder()
+                    .code(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                    .message(violations)
+                    .build();
+        } else {
+            String exceptionMessage = validationException.getMessage();
+            log.error(exceptionMessage, validationException);
+            errorDTO = ErrorDTO.builder()
+                    .code(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                    .message(exceptionMessage)
+                    .build();
+        }
+        return errorDTO;
+    }
+
+    private String extractViolationsFromException(ConstraintViolationException validationException) {
+        return validationException.getConstraintViolations()
+                .stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(Collectors.joining("--"));
+    }
+}
