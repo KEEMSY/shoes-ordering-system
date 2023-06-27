dd7c32003137a082b37ca012ede8dd7395884529
keemsy <phzzz@naver.com>
2023-06-27 10:39:30 +0900

    Update: ProductCategory 업데이트 - 비활성 카테고리 추가

--- src/main/java/com/shoes/ordering/system/domains/product/domain/core/valueobject/ProductCategory.java
+++ src/main/java/com/shoes/ordering/system/domains/product/domain/core/valueobject/ProductCategory.java
@@ -1,5 +1,5 @@
 package com.shoes.ordering.system.domains.product.domain.core.valueobject;
 
 public enum ProductCategory {
-    SHOES, CLOTHING
+    SHOES, CLOTHING, DISABLING
 }
--- src/main/java/com/shoes/ordering/system/domains/product/domain/core/valueobject/ProductImageId.java
+++ src/main/java/com/shoes/ordering/system/domains/product/domain/core/valueobject/ProductImageId.java
@@ -5,7 +5,7 @@ import com.shoes.ordering.system.domains.common.valueobject.BaseId;
 import java.util.UUID;
 
 public class ProductImageId extends BaseId<UUID> {
-    protected ProductImageId(UUID value) {
+    public ProductImageId(UUID value) {
         super(value);
     }
 }
