5d2b7c72901f7464a56edd937206f65d8c9f0d09
keemsy <phzzz@naver.com>
2023-06-29 22:37:35 +0900

    Add: CreateProductCommand 및 CreateProductCommandTest 추가 - 정상 생성 및 입력 유효성 검층 테스트 추가

---
+++ src/test/java/com/shoes/ordering/system/domains/product/domain/application/create/CreateProductCommandTest.java
@@ -0,0 +1,64 @@
+package com.shoes.ordering.system.domains.product.domain.application.create;
+
+import com.shoes.ordering.system.domains.common.valueobject.Money;
+import com.shoes.ordering.system.domains.product.domain.application.dto.create.CreateProductCommand;
+import com.shoes.ordering.system.domains.product.domain.core.valueobject.ProductCategory;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolationException;
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class CreateProductCommandTest {
+
+    @Test
+    @DisplayName("정상 CreateProductCommand 생성")
+    public void createProductCommandTest1() {
+
+        // given
+        String testProductName = "Test ProductName";
+        ProductCategory testProductCategory = ProductCategory.SHOES;
+        String testProductDescription = "Test Product Description";
+        Money testProductPrice = new Money(new BigDecimal("200.00"));
+        List<String> testProductImages = List.of("testURL1", "testURL2");
+
+        // when
+        CreateProductCommand createProductCommand = CreateProductCommand.builder()
+                .name(testProductName)
+                .productCategory(testProductCategory)
+                .description(testProductDescription)
+                .price(testProductPrice)
+                .productImages(testProductImages)
+                .build();
+
+        // then
+        assertThat(createProductCommand).isNotNull();
+        assertThat(createProductCommand.getName()).isEqualTo(testProductName);
+    }
+
+    @Test
+    @DisplayName("비정상 CreateProductCommand 생성: 프로퍼티에 값에는 Null 이 올 수 없다.")
+    public void createProductCommandTest2() {
+
+        //given
+        String testProductName = "Test ProductName";
+        ProductCategory testProductCategory = ProductCategory.SHOES;
+        String testProductDescription = "Test Product Description";
+        Money testProductPrice = new Money(new BigDecimal("200.00"));
+        List<String> testProductImages = List.of("testURL1", "testURL2");
+
+        //when, then
+        assertThatThrownBy(() -> CreateProductCommand.builder()
+                .name(null) // name 프로퍼티 누락
+                .productCategory(testProductCategory)
+                .description(testProductDescription)
+                .price(testProductPrice)
+                .productImages(testProductImages)
+                .build()
+        ).isInstanceOf(ConstraintViolationException.class);
+    }
+}
