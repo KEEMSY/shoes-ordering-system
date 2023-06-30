d4457517e3c6dd7293ab2184b694d35b902a62d0
keemsy <phzzz@naver.com>
2023-06-30 23:45:27 +0900

    Update: ProductImage 내 @Getter 추가

--- src/main/java/com/shoes/ordering/system/domains/product/domain/core/entity/ProductImage.java
+++ src/main/java/com/shoes/ordering/system/domains/product/domain/core/entity/ProductImage.java
@@ -1,7 +1,9 @@
 package com.shoes.ordering.system.domains.product.domain.core.entity;
 
 import com.shoes.ordering.system.domains.product.domain.core.valueobject.ProductImageId;
+import lombok.Getter;
 
+@Getter
 public class ProductImage {
 
     private final ProductImageId productImageId;
