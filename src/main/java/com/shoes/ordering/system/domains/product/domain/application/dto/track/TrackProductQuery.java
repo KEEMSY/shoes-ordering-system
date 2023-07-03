b031ac190f3bd25bbf0d4fd902fa5d8ba80357ef
keemsy <phzzz@naver.com>
2023-07-03 20:31:12 +0900

    Implement: TrackProductQuery 구현 - TrackProductQueryTest 추가

--- src/main/java/com/shoes/ordering/system/domains/product/domain/application/dto/track/TrackProductQuery.java
+++ src/main/java/com/shoes/ordering/system/domains/product/domain/application/dto/track/TrackProductQuery.java
@@ -1,4 +1,35 @@
 package com.shoes.ordering.system.domains.product.domain.application.dto.track;
 
-public class TrackProductQuery {
+import com.shoes.ordering.system.domains.common.validation.SelfValidating;
+import lombok.Getter;
+
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+@Getter
+public class TrackProductQuery extends SelfValidating<TrackProductQuery> {
+
+    @NotNull private final UUID productId;
+
+    private TrackProductQuery(Builder builder) {
+        productId = builder.productId;
+
+        this.validateSelf(this);
+    }
+    public static Builder builder() {
+        return new Builder();
+    }
+    public static final class Builder {
+        private @NotNull UUID productId;
+
+        private Builder() {
+        }
+        public Builder productId(@NotNull UUID val) {
+            productId = val;
+            return this;
+        }
+        public TrackProductQuery build() {
+            return new TrackProductQuery(this);
+        }
+    }
 }
