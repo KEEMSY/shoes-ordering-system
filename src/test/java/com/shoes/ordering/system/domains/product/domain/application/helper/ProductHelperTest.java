9cf0327646eab488bffbb3e30afbc097053f3543
keemsy <phzzz@naver.com>
2023-07-06 13:25:54 +0900

    Update: ProductHelperTest 테스트 개선 - init 메서드 개선 및 기존 테스트 given 섹션 개선

--- src/test/java/com/shoes/ordering/system/domains/product/domain/application/helper/ProductHelperTest.java
+++ src/test/java/com/shoes/ordering/system/domains/product/domain/application/helper/ProductHelperTest.java
@@ -4,12 +4,14 @@ import com.shoes.ordering.system.TestConfiguration;
 import com.shoes.ordering.system.domains.common.valueobject.Money;
 import com.shoes.ordering.system.domains.product.domain.application.dto.create.CreateProductCommand;
 import com.shoes.ordering.system.domains.product.domain.application.dto.update.UpdateProductCommand;
-import com.shoes.ordering.system.domains.product.domain.application.mapper.ProductDataMapper;
 import com.shoes.ordering.system.domains.product.domain.application.ports.output.repository.ProductRepository;
 import com.shoes.ordering.system.domains.product.domain.core.entity.Product;
+import com.shoes.ordering.system.domains.product.domain.core.entity.ProductImage;
 import com.shoes.ordering.system.domains.product.domain.core.event.ProductCreatedEvent;
 import com.shoes.ordering.system.domains.product.domain.core.event.ProductUpdatedEvent;
 import com.shoes.ordering.system.domains.product.domain.core.valueobject.ProductCategory;
+import com.shoes.ordering.system.domains.product.domain.core.valueobject.ProductId;
+import com.shoes.ordering.system.domains.product.domain.core.valueobject.ProductImageId;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,6 +22,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -31,8 +34,6 @@ public class ProductHelperTest {
     @Autowired
     private ProductRepository productRepository;
     @Autowired
-    private ProductDataMapper productDataMapper;
-    @Autowired
     private ProductHelper productHelper;
 
     private Product product;
@@ -41,26 +42,16 @@ public class ProductHelperTest {
 
     @BeforeEach
     public void init() {
-         createProductCommand = CreateProductCommand.builder()
-                .name("TestName")
+        product = Product.builder()
+                .productId(new ProductId(UUID.randomUUID()))
+                .name("Test name")
                 .productCategory(ProductCategory.SHOES)
                 .description("Test Description")
                 .price(new Money(new BigDecimal("200.00")))
-                .productImages(List.of("testURL1", "testURL2"))
+                .productImages(List.of(new ProductImage(new ProductImageId(UUID.randomUUID()), "TestUrl")))
                 .build();
-
-        product = productDataMapper.creatProductCommandToProduct(createProductCommand);
         product.initializeProduct();
         product.validateProduct();
-
-        updateProductCommand = UpdateProductCommand.builder()
-                 .productId(product.getId().getValue())
-                 .name("UpdateTestName")
-                 .productCategory(ProductCategory.SHOES)
-                 .description("Update Test Description")
-                 .price(new Money(new BigDecimal("100.00")))
-                 .productImages(List.of("testURL1", "testURL2"))
-                 .build();
 
          when(productRepository.save(any(Product.class))).thenReturn(product);
          when(productRepository.findByProductId(product.getId().getValue())).thenReturn(Optional.ofNullable(product));
@@ -69,7 +60,14 @@ public class ProductHelperTest {
     @Test
     @DisplayName("정상 CreateProductEvent 생성 확인")
     public void persistProductTest() {
-        // given: BeforeEach 내 포함
+        // given
+        createProductCommand = CreateProductCommand.builder()
+                .name("TestName")
+                .productCategory(ProductCategory.SHOES)
+                .description("Test Description")
+                .price(new Money(new BigDecimal("200.00")))
+                .productImages(List.of("TestUrl"))
+                .build();
 
         // when
         ProductCreatedEvent resultProductCreatedEvent = productHelper.persistProduct(createProductCommand);
@@ -83,7 +81,15 @@ public class ProductHelperTest {
     @Test
     @DisplayName("정상 UpdateProductEvent 생성 확인")
     public void updateProductPersistTest() {
-        // given: BeforeEach 에 포함
+        // given
+        updateProductCommand = UpdateProductCommand.builder()
+                .productId(product.getId().getValue())
+                .name("UpdateTestName")
+                .productCategory(ProductCategory.SHOES)
+                .description("Update Test Description")
+                .price(new Money(new BigDecimal("100.00")))
+                .productImages(List.of("testURL1", "testURL2"))
+                .build();
 
         // when
         ProductUpdatedEvent resultProductUpdatedEvent = productHelper.updateProductPersist(updateProductCommand);
