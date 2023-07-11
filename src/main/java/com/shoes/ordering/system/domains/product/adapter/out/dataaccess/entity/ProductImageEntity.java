04996a432e465f9479006a0c86edc5de57394a28
keemsy <phzzz@naver.com>
2023-07-11 21:37:09 +0900

    Chore: ProductImageEntity 내 오타 수정

--- src/main/java/com/shoes/ordering/system/domains/product/adapter/out/dataaccess/entity/ProductImageEntity.java
+++ src/main/java/com/shoes/ordering/system/domains/product/adapter/out/dataaccess/entity/ProductImageEntity.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "product_imags")
+@Table(name = "product_images")
 @Entity
 public class ProductImageEntity {
     @Id
