1125840d353bb0f6a3da37643e3aa2de0996c70a
keemsy <phzzz@naver.com>
2023-07-13 18:24:51 +0900

    Update: ProductPersistenceAdapterTest 개선

--- src/test/java/com/shoes/ordering/system/domains/product/adapter/out/dataaccess/adapter/ProductPersistenceAdapterTest.java
+++ src/test/java/com/shoes/ordering/system/domains/product/adapter/out/dataaccess/adapter/ProductPersistenceAdapterTest.java
@@ -1,11 +1,14 @@
 package com.shoes.ordering.system.domains.product.adapter.out.dataaccess.adapter;
 
 import com.shoes.ordering.system.TestConfiguration;
+import com.shoes.ordering.system.domains.common.valueobject.Money;
 import com.shoes.ordering.system.domains.product.adapter.out.dataaccess.entity.ProductEntity;
 import com.shoes.ordering.system.domains.product.adapter.out.dataaccess.mapper.ProductDataAccessMapper;
 import com.shoes.ordering.system.domains.product.adapter.out.dataaccess.respository.ProductJpaRepository;
 import com.shoes.ordering.system.domains.product.adapter.out.dataaccess.respository.ProductQuerydslRepository;
+import com.shoes.ordering.system.domains.product.domain.core.entity.Product;
 import com.shoes.ordering.system.domains.product.domain.core.valueobject.ProductCategory;
+import com.shoes.ordering.system.domains.product.domain.core.valueobject.ProductId;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,6 +26,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ProductPersistenceAdapterTest {
 
     @Autowired
+    ProductPersistenceAdapter productPersistenceAdapter;
+    @Autowired
     ProductQuerydslRepository productQuerydslRepository;
     @Autowired
     ProductJpaRepository productJpaRepository;
@@ -33,23 +38,33 @@ class ProductPersistenceAdapterTest {
     public void clean() {
         productJpaRepository.deleteAll();
     }
-    private ProductEntity createProductEntity(UUID productId, String name) {
+    private ProductEntity createProductEntity(UUID productId, String name, ProductCategory productCategory) {
         return ProductEntity.builder()
                 .productId(productId)
-                .productCategory(ProductCategory.SHOES)
+                .productCategory(productCategory)
                 .name(name)
                 .description("Test Product Description")
                 .price(new BigDecimal("200.00"))
+                .build();
+    }
+
+    private Product createProduct(ProductId productId, String name, ProductCategory productCategory) {
+        return Product.builder()
+                .productId(productId)
+                .productCategory(productCategory)
+                .name(name)
+                .description("Test Product Description")
+                .price(new Money(new BigDecimal("200.00")))
                 .build();
     }
     @Test
     @DisplayName("정상 save 테스트")
     void saveTest() {
         // given
-        ProductEntity productEntity = createProductEntity(UUID.randomUUID(), "TestName");
+        Product product = createProduct(new ProductId(UUID.randomUUID()), "TestName", ProductCategory.SHOES);
 
         // when
-        productJpaRepository.save(productEntity);
+        productPersistenceAdapter.save(product);
 
         // then
         List<ProductEntity> results = productJpaRepository.findAll();
@@ -64,13 +79,13 @@ class ProductPersistenceAdapterTest {
         // given
         UUID productId = UUID.randomUUID();
 
-        ProductEntity productEntity1 = createProductEntity(productId, "TestName1");
-        ProductEntity productEntity2 = createProductEntity(UUID.randomUUID(), "TestName2");
+        ProductEntity productEntity1 = createProductEntity(productId, "TestName1", ProductCategory.SHOES);
+        ProductEntity productEntity2 = createProductEntity(UUID.randomUUID(), "TestName2", ProductCategory.CLOTHING);
 
         productJpaRepository.saveAll(List.of(productEntity1, productEntity2));
 
         // when
-        Optional<ProductEntity> result = productJpaRepository.findByProductId(productEntity1.getProductId());
+        Optional<Product> result = productPersistenceAdapter.findByProductId(productEntity1.getProductId());
 
         // then
         assertThat(result).isPresent();
