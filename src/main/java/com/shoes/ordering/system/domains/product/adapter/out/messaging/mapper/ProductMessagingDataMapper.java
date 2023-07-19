37d0a240e639f0920af13c2b80be06f8c7d4bf44
keemsy <phzzz@naver.com>
2023-07-19 20:21:12 +0900

    Implement: UpdateProductKafkaMessagePublisher 추가 - ProductUpdatedRequestMessagePublisher 구현 - ProductMessagingDataMapper 내 메서드 추가

--- src/main/java/com/shoes/ordering/system/domains/product/adapter/out/messaging/mapper/ProductMessagingDataMapper.java
+++ src/main/java/com/shoes/ordering/system/domains/product/adapter/out/messaging/mapper/ProductMessagingDataMapper.java
@@ -2,8 +2,10 @@ package com.shoes.ordering.system.domains.product.adapter.out.messaging.mapper;
 
 import com.shoes.ordering.system.CreateProductRequestAvroModel;
 import com.shoes.ordering.system.ProductCategory;
+import com.shoes.ordering.system.UpdateProductRequestAvroModel;
 import com.shoes.ordering.system.domains.product.domain.core.entity.Product;
 import com.shoes.ordering.system.domains.product.domain.core.event.ProductCreatedEvent;
+import com.shoes.ordering.system.domains.product.domain.core.event.ProductUpdatedEvent;
 import com.shoes.ordering.system.domains.product.domain.core.exception.ProductDomainException;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +21,18 @@ public class ProductMessagingDataMapper {
                 .setProductCategory(domainProductCategoryToAvroProductKind(product.getProductCategory()))
                 .setPrice(product.getPrice().getAmount())
                 .setCreatedAt(productCreatedEvent.getCreatedAt().toInstant())
+                .build();
+    }
+
+    public UpdateProductRequestAvroModel productUpdatedProductRequestAvroModel(ProductUpdatedEvent productUpdatedEvent) {
+        Product product = productUpdatedEvent.getProduct();
+        return UpdateProductRequestAvroModel.newBuilder()
+                .setProductId(product.getId().getValue().toString())
+                .setName(product.getName())
+                .setDescription(product.getDescription())
+                .setProductCategory(domainProductCategoryToAvroProductKind(product.getProductCategory()))
+                .setPrice(product.getPrice().getAmount())
+                .setCreatedAt(productUpdatedEvent.getCreatedAt().toInstant())
                 .build();
     }
 
