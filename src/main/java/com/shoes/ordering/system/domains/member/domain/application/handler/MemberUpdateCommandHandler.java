5642d79c647f5aba2c7b409418d00ef3eb265434
keemsy <phzzz@naver.com>
2023-08-07 23:53:07 +0900

    Fix: MemberUpdateCommandHandler 의 로직 수정 - updateMember() 시, 정상적으로 MemberUpdatedEvent 를 발행하도록 수정 - 테스트 추가

--- src/main/java/com/shoes/ordering/system/domains/member/domain/application/handler/MemberUpdateCommandHandler.java
+++ src/main/java/com/shoes/ordering/system/domains/member/domain/application/handler/MemberUpdateCommandHandler.java
@@ -4,6 +4,7 @@ import com.shoes.ordering.system.domains.member.domain.application.dto.update.Up
 import com.shoes.ordering.system.domains.member.domain.application.dto.update.UpdateMemberResponse;
 import com.shoes.ordering.system.domains.member.domain.application.helper.MemberHelper;
 import com.shoes.ordering.system.domains.member.domain.application.mapper.MemberDataMapper;
+import com.shoes.ordering.system.domains.member.domain.application.ports.output.message.publisher.MemberUpdateRequestMessagePublisher;
 import com.shoes.ordering.system.domains.member.domain.core.event.MemberUpdatedEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -12,16 +13,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class MemberUpdateCommandHandler {
     private final MemberHelper memberHelper;
+    private final MemberUpdateRequestMessagePublisher memberUpdateRequestMessagePublisher;
     private final MemberDataMapper memberDataMapper;
 
-    public MemberUpdateCommandHandler(MemberHelper memberHelper, MemberDataMapper memberDataMapper) {
+    public MemberUpdateCommandHandler(MemberHelper memberHelper,
+                                      MemberUpdateRequestMessagePublisher memberUpdateRequestMessagePublisher,
+                                      MemberDataMapper memberDataMapper) {
         this.memberHelper = memberHelper;
+        this.memberUpdateRequestMessagePublisher = memberUpdateRequestMessagePublisher;
         this.memberDataMapper = memberDataMapper;
     }
 
     public UpdateMemberResponse updateMember(UpdateMemberCommand updateMemberCommand) {
         MemberUpdatedEvent memberUpdatedEvent = memberHelper.updateMemberPersist(updateMemberCommand);
         log.info("Member is updated with id: {}", memberUpdatedEvent.getMember().getId().getValue());
+        memberUpdateRequestMessagePublisher.publish(memberUpdatedEvent);
         return memberDataMapper.memberToUpdateMemberResponse(memberUpdatedEvent.getMember());
     }
 }
