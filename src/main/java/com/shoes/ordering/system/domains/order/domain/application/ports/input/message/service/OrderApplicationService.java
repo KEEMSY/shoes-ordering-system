5986a6194fda23fd94884c842bf71dffb6de386b
keemsy <phzzz@naver.com>
2023-08-14 16:55:26 +0900

    Add: OrderApplicationService 추가

---
+++ src/main/java/com/shoes/ordering/system/domains/order/domain/application/ports/input/message/service/OrderApplicationService.java
@@ -0,0 +1,15 @@
+package com.shoes.ordering.system.domains.order.domain.application.ports.input.message.service;
+
+import com.shoes.ordering.system.domains.order.domain.application.dto.create.CreateOrderCommand;
+import com.shoes.ordering.system.domains.order.domain.application.dto.create.CreateOrderResponse;
+import com.shoes.ordering.system.domains.order.domain.application.dto.track.TrackOrderQuery;
+import com.shoes.ordering.system.domains.order.domain.application.dto.track.TrackOrderResponse;
+
+import javax.validation.Valid;
+
+public interface OrderApplicationService {
+
+    CreateOrderResponse createOrder(@Valid CreateOrderCommand createOrderCommand);
+
+    TrackOrderResponse trackOrder(@Valid TrackOrderQuery trackOrderQuery);
+}
