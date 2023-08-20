1ff8da746b13680d1e1e0a8740b0edd5a5ed03ad
keemsy <phzzz@naver.com>
2023-08-20 17:36:01 +0900

    Add: CreateOrderKafkaMessagePublisher 추가 - 관련 모듈(mapper, helper) 추가 - 테스트 추가

---
+++ src/main/java/com/shoes/ordering/system/domains/order/adapter/out/messaging/mapper/OrderMessagingDataMapper.java
@@ -0,0 +1,41 @@
+package com.shoes.ordering.system.domains.order.adapter.out.messaging.mapper;
+
+import com.shoes.ordering.system.PaymentOrderStatus;
+import com.shoes.ordering.system.PaymentRequestAvroModel;
+import com.shoes.ordering.system.PaymentResponseAvroModel;
+import com.shoes.ordering.system.domains.order.domain.application.dto.message.PaymentResponse;
+import com.shoes.ordering.system.domains.order.domain.core.entity.Order;
+import com.shoes.ordering.system.domains.order.domain.core.event.OrderCreatedEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class OrderMessagingDataMapper {
+    public PaymentRequestAvroModel orderCreatedEventToPaymentRequestAvroModel(OrderCreatedEvent orderCreatedEvent) {
+        Order order = orderCreatedEvent.getOrder();
+        return PaymentRequestAvroModel.newBuilder()
+                .setId(UUID.randomUUID())
+                .setMemberId(order.getMemberId().getValue())
+                .setOrderId(order.getId().getValue())
+                .setPrice(order.getPrice().getAmount())
+                .setCreatedAt(orderCreatedEvent.getCreatedAt().toInstant())
+                .setPaymentOrderStatus(PaymentOrderStatus.PENDING)
+                .build();
+    }
+
+    public PaymentResponse paymentResponseAvroModelToPaymentResponse(PaymentResponseAvroModel
+                                                                             paymentResponseAvroModel) {
+        return PaymentResponse.builder()
+                .id(paymentResponseAvroModel.getId().toString())
+                .paymentId(paymentResponseAvroModel.getPaymentId().toString())
+                .memberId(paymentResponseAvroModel.getMemberId().toString())
+                .orderId(paymentResponseAvroModel.getOrderId().toString())
+                .price(paymentResponseAvroModel.getPrice())
+                .createdAt(paymentResponseAvroModel.getCreatedAt())
+                .paymentStatus(com.shoes.ordering.system.domains.common.valueobject.PaymentStatus.valueOf(
+                        paymentResponseAvroModel.getPaymentStatus().name()))
+                .failureMessages(paymentResponseAvroModel.getFailureMessages())
+                .build();
+    }
+}
