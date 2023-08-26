529e7624c41ac358033db13e1092548531c14d77
keemsy <phzzz@naver.com>
2023-08-27 00:24:56 +0900

    Fix: 도메인 별 GlobalExceptionHandler 개선 - 도메인 별 존재하는 GlobalExceptionHandler 에 대하여, 적용 법위 지정

--- src/main/java/com/shoes/ordering/system/domains/order/adapter/in/controller/exception/handler/OrderGlobalExceptionHandler.java
+++ src/main/java/com/shoes/ordering/system/domains/order/adapter/in/controller/exception/handler/OrderGlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import com.shoes.ordering.system.common.handler.ErrorDTO;
 import com.shoes.ordering.system.common.handler.GlobalExceptionHandler;
 import com.shoes.ordering.system.domains.order.domain.core.exception.OrderDomainException;
 import com.shoes.ordering.system.domains.order.domain.core.exception.OrderNotFoundException;
-import com.shoes.ordering.system.domains.product.domain.core.exception.ProductNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
@@ -17,14 +16,14 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import javax.validation.ConstraintViolationException;
 
 @Slf4j
-@ControllerAdvice
+@ControllerAdvice(basePackages = "com.shoes.ordering.system.domains.order.adapter.in.controller.rest")
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class OrderGlobalExceptionHandler extends GlobalExceptionHandler {
 
     @ResponseBody
     @ExceptionHandler({OrderDomainException.class, ConstraintViolationException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorDTO handleProductException(Exception ex) {
+    public ErrorDTO handleOrderException(Exception ex) {
         log.error(ex.getMessage(), ex);
         return ErrorDTO.builder()
                 .code(HttpStatus.BAD_REQUEST.getReasonPhrase())
@@ -35,7 +34,7 @@ public class OrderGlobalExceptionHandler extends GlobalExceptionHandler {
     @ResponseBody
     @ExceptionHandler(OrderNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorDTO handleProductNotFoundException(ProductNotFoundException ex) {
+    public ErrorDTO handleOrderNotFoundException(OrderNotFoundException ex) {
         log.error(ex.getMessage(), ex);
         return ErrorDTO.builder()
                 .code(HttpStatus.NOT_FOUND.getReasonPhrase())
