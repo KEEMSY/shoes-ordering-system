0b605f4c71275966f7df09dced135c39ff71d793
keemsy <phzzz@naver.com>
2023-10-31 18:00:26 +0900

    Add: Payment 도메인 내 사용하는 Exception 추가 - DomainException, NotFoundException 추가

---
+++ src/main/java/com/shoes/ordering/system/domains/payment/domain/core/exception/PaymentDomainException.java
@@ -0,0 +1,13 @@
+package com.shoes.ordering.system.domains.payment.domain.core.exception;
+
+import com.shoes.ordering.system.domains.common.exception.DomainException;
+
+public class PaymentDomainException extends DomainException {
+    public PaymentDomainException(String message) {
+        super(message);
+    }
+
+    public PaymentDomainException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
